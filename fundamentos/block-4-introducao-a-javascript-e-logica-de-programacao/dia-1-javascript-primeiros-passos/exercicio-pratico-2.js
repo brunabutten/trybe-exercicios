6a586a6df165a0df0285e5ff82f66e542f48b5fb
bruna <brunabutten@gmail.com>
2021-11-28 20:15:36 -0300

    Adicionando exercícios do Bloco 04 - dia 01

---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-pratico-2.js
@@ -0,0 +1,8 @@
+const a = 10;
+const b = 2;
+
+if (a > b) {
+    console.log("'a' é maior que 'b'");
+} else {
+    console.log("'b' é maior que 'a'");
+};
---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-pratico-9.js
@@ -0,0 +1,10 @@
+const a = 2;
+const b = 9;
+const c = 7;
+
+let number = false;
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
+  number = true;
+};
+console.log(number);
