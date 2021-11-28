1d5a9ee6ecab9cfb1598ddad6d7ab1c59c6406c0
bruna <brunabutten@gmail.com>
2021-11-28 16:48:51 -0300

    Começando o dia

---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,7 @@
+const myName = "Bruna Büttenbender";
+const birthCity = "Florianópolis";
+let birthYear = 1989;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio2.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof  firstName);
+console.log(typeof lastName);
+console.log(typeof patientEmail);
+
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = area * 2;
+
+console.log(area);
+console.log(perimeter);
