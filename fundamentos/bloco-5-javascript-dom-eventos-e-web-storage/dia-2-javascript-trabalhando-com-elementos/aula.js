51b50fcd14b4b22cad265aedc4a8268cc72f9b78
bruna <brunabutten@gmail.com>
2022-01-21 18:26:26 -0300

    Adicionando exercícios

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/aula.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/aula.js
@@ -1,21 +1,21 @@
-// Método de capturar elementos da página
+// Métodos de capturar elementos da página
 let second = document.getElementById('second');
 
-// Criar elemento HTML via Javascript
+// Criar elemento HTML via JavaScript
 let newElement = document.createElement('div');
 
 // Configurar elemento
-newElement.className = "red circle small";
+newElement.className = 'red circle small';
 
 // elemento.className vs elemento.classList.add()
 // newElement.classList.add('green');
 // newElement.classList.add('square');
 // newElement.classList.add('small');
 
-// Adiciona elemento na página
+// adicionar elemento na página
 second.appendChild(newElement);
 
-function appendDrawing (parent, classes) {
+function appendDrawing(parent, classes) {
     let child = document.createElement('div');
     child.className = classes;
     parent.appendChild(child);
@@ -26,50 +26,53 @@ appendDrawing(second, 'yellow circle small');
 appendDrawing(second, 'red circle medium');
 appendDrawing(second, 'red square medium');
 
-
-// Adicionar um quadrado amarelo médio
+// // adicionar um quadrado amarelo medio
 // let yellowSquare = document.createElement('div');
-// yellowSquare.className = 'yellow square medium'
+// yellowSquare.className = "yellow square medium";
 // second.appendChild(yellowSquare);
 
 // let greenSquare = document.createElement('div');
-// greenSquare.className = 'yellow square small'
+// greenSquare.className = "green square small";
 // second.appendChild(greenSquare);
 
 // let yellowCircle = document.createElement('div');
-// yellowCircle.className = 'yellow circle medium'
+// yellowCircle.className = "yellow circle medium";
 // second.appendChild(yellowCircle);
 
-
-// Adicionar um círculo verde grande no terceiro
-
-// let greenCircle = document.createElement('div');
-// greenCircle.className = 'green circle big';
-// third.appendChild(greenCircle);
-
+// adicionar um círculo verde grande no terceiro
 let third = document.getElementById('third');
 
 appendDrawing(third, 'green circle big');
 appendDrawing(third, 'red square medium');
 
+// let greenCircle = document.createElement('div');
+// greenCircle.className = "green circle big";
+// third.appendChild(greenCircle);
 
-// Encontrar primeiro / último elemento filho
+// encontrar primeiro/último elemento filho
 // second.firstElementChild
 // second.lastElementChild
 
-// Encontrar próximo elemento irmão
-// let element = second.firstElementChild // Inicializa a repetição
-// element = element.nextElementSibling // Incrementa para o próximo elemento
+// encontrar próximo elemento irmão
+// let element = second.firstElementChild // inicializa
+// element = element.nextElementSibling // incrementa
 
 // null é falsy
 
-// Percorrer filhos
+// percorrer filhos
+// 
+
 let count = 0;
-for (let element = second.firstElementChild; element !=+ null; element = element.nextElementSibling) {
+for (
+    let element = second.firstElementChild;
+    element !== null;
+    element = element.nextElementSibling
+) {
     console.log(element);
     count += 1;
 }
 console.log(count);
+
 
 
 // removeChild
@@ -79,5 +82,5 @@ let array = [
     'green square small',
     'yellow circle medium',
     'red square small',
-    'green circle medium',
+    'green circle medium'
 ]
