5db03945d129e6b9053d819e1a778a04fbacb391
bruna <brunabutten@gmail.com>
2022-01-30 22:33:06 -0300

    Adicionando exercícios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerc2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerc2.js
@@ -2,8 +2,8 @@
 Dica: use parâmetro rest . */
 
 // escreva sum abaixo
-/* const sum = (...numbers) => numbers.reduce(((acc, val) => acc + val), 0); */
 
 
+const sum = (...numbers) => numbers.reduce(((acc, val) => acc + val), 0);
 
-
+console.log(sum(1, 2, 3));
