bc33073288f5f5b182cf1354a285f4d12cc56dfd
bruna <brunabutten@gmail.com>
2022-01-31 21:24:42 -0300

    Adicionando exercícios

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,115 @@
+// api.js
+const fetchCoins = async () => {
+    const url = 'https://api.coincap.io/v2/assets';
+
+    const coins = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.data)
+        .catch((error) => error.toString());
+
+    return coins;
+}
+
+const setCoins = async () => {
+    const coins = await fetchCoins();
+
+    const coinsList = document.getElementById('coins-list');
+
+    coins
+        .filter((coin) => Number(coin.rank) <= 10)
+        .forEach((coin) => {
+            const newLi = document.createElement('li');
+
+            newLi.innerText = `${coin.name} (${coin.symbol}): ${coin.priceUsd}`;
+
+            coinsList.appendChild(newLi);
+        });
+}
+
+window.onload = () => setCoins();
+
+/* Nessa solução utilizamos a propriedade `rank` da moeda para fazer o filtro das 10 primeiras. */
+
+
+// api.js
+/* const fetchCoins = async () => {
+    const url = 'https://api.coincap.io/v2/assets';
+  
+    const coins = await fetch(url)
+      .then((response) => response.json())
+      .then((data) => data.data)
+      .catch((error) => error.toString());
+  
+    return coins;
+  }
+  
+  const setCoins = async () => {
+    const coins = await fetchCoins();
+  
+    const coinsList = document.getElementById('coins-list');
+  
+    coins
+      .filter((_, index) => index < 10)
+      .forEach((coin) => {
+        const newLi = document.createElement('li');
+  
+        newLi.innerText = `${coin.name} (${coin.symbol}): ${coin.priceUsd}`;
+  
+        coinsList.appendChild(newLi);
+      });
+  }
+  
+  window.onload = () => setCoins(); */
+
+/*   Já nessa solução utilizamos o index disponível pela HOF `filter` para pegar os 10 primeiros valores do array de moedas. */
+
+
+/* BONUS */
+
+/* Que tal usarmos uma API para converter o preço das crypto moedas do exercício anterior para a nossa moeda local ao invés de mostrar o seu valor em dólar? */
+
+
+/* const fetchCoins = async () => {
+    const url = 'https://api.coincap.io/v2/assets';
+
+    const coins = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.data)
+        .catch((error) => error.toString());
+
+    return coins;
+};
+
+const fetchUsdCurrencies = async () => {
+    const baseUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest'
+    const usdEndpoint = '/currencies/usd.min.json'
+    const url = baseUrl.concat(usdEndpoint);
+
+    const usdCurrencies = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.usd)
+        .catch((error) => error.toString());
+
+    return usdCurrencies;
+};
+
+const setCoins = async () => {
+    const coins = await fetchCoins();
+    const usdCurrencies = await fetchUsdCurrencies();
+
+    const coinsList = document.getElementById('coins-list');
+
+    coins
+        .filter((coin) => Number(coin.rank) <= 10)
+        .forEach((coin) => {
+            const newLi = document.createElement('li');
+            const usdPrice = Number(coin.priceUsd);
+            const brlPrice = usdPrice * usdCurrencies.brl;
+
+            newLi.innerText = `${coin.name} (${coin.symbol}): ${brlPrice.toFixed(2)}`;
+
+            coinsList.appendChild(newLi);
+        });
+};
+
+window.onload = () => setCoins(); */
